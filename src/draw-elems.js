90cb4a706f15f0d5e86e5f31ab85dff253213565
GlebOgl <glebogloblin07@gmail.com>
2023-06-13 18:05:38 +0500

    Update draw-elems.js

--- src/draw-elems.js
+++ src/draw-elems.js
@@ -6,8 +6,6 @@ import {NewEventView} from './view/event-view.js';
 import {NewElemListView} from './view/events-list-view.js';
 import {NewEventEditorView} from './view/edit-event-view.js';
 import {render, RenderPosition} from './render.js';
-import {PointModel} from './model/point-model.js';
-import {getDefaultPoint} from './util.js';
 
 class ElemsDrawer {
   #headerContainer = null;
@@ -55,6 +53,7 @@ class ElemsDrawer {
     const onEscKey = (evt) => {
       if (evt.key === 'Escape'){
         turnPointToView();
+        document.removeEventListener('keyup', onEscKey);
       }
     };
 
@@ -65,66 +64,21 @@ class ElemsDrawer {
 
     pointEditComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
       turnPointToView();
-      document.removeEventListener('keyup', onEscKey)
+      document.removeEventListener('keyup', onEscKey);
     });
 
     pointEditComponent.element.querySelector('.event--edit').addEventListener('submit', () => {
       turnPointToView();
-      document.removeEventListener('keyup', onEscKey)
+      document.removeEventListener('keyup', onEscKey);
     });
 
     pointEditComponent.element.querySelector('.event--edit').addEventListener('reset', () => {
       turnPointToView();
-      document.removeEventListener('keyup', onEscKey)
+      document.removeEventListener('keyup', onEscKey);
     });
 
     render(pointComponent, this.#eventsList.element);
   };
-
 }
-
-// const renderPoint = (point, destinations, offers, eventsList) => {
-//   const pointComponent = new NewEventView(point, destinations, offers);
-//   const pointEditComponent = new NewEventEditorView(point, destinations, offers);
-//   const turnPointToEdit = () => {
-//     eventsList[0].replaceChild(pointEditComponent.element, pointComponent.element);
-//   };
-//   const turnPointToView = () => {
-//     eventsList[0].replaceChild(pointComponent.element, pointEditComponent.element);
-//   };
-//   render(pointComponent, eventsList[0]);
-// };
-
-// const init = () => {
-//   const headerTrip = document.querySelector('.trip-main');
-//   const siteMenuElement = headerTrip.querySelector('.trip-controls__navigation');
-
-//   const siteFilterElement = headerTrip.querySelector('.trip-controls__filters');
-
-//   const mainEvents = document.querySelector('.trip-events');
-
-//   const pointModel = new PointModel();
-//   const points = pointModel.getPoints();
-//   const destinations = pointModel.getDestinations();
-//   const offersByType = pointModel.getOffersByType();
-
-//   render(new NewInfoView(), headerTrip, RenderPosition.AFTERBEGIN);
-//   render(new NewNavView(), siteMenuElement);
-//   render(new NewFilterView(), siteFilterElement);
-//   render(new NewSortView(), mainEvents);
-
-//   render(new NewElemListView(), mainEvents);
-
-//   const eventsList = mainEvents.getElementsByClassName('trip-events__list');
-
-//   points.forEach((point) => {
-//     renderPoint(point, destinations, offersByType, eventsList[0]);
-//   });
-
-//   //const events = mainEvents.getElementsByClassName('trip-events__item');
-
-//   //render(new NewEventEditorView(getDefaultPoint(), destinations, offersByType), eventsList[0], RenderPosition.AFTERBEGIN);
-//   //render(new NewEventEditorView(points[0], destinations, offersByType), events[1], RenderPosition.AFTEREND);
-// };
 
 export {ElemsDrawer};
